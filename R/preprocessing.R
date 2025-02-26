3c61ead187b89f3719129859c2a28b0f347adf2b
JMartinezGarcia <javier.nezcia@gmail.com>
2025-02-26 13:58:08 +0100

    Added test for preprocessing functions

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -5,15 +5,66 @@ create_recipe <- function(formula, data){
   return(rec)
 }
 
+standarize_predictors <- function(rec, norm_num_vars){
+
+  if (norm_num_vars == "all"){
+
+    rec <- recipes::step_normalize(rec, recipes::all_numeric_predictors())
+
+  } else{
+
+    rec <- recipes::step_normalize(rec, all_of(norm_num_vars))
+
+  }
+
+  return(rec)
+
+}
+
+one_hot_predictors <- function(rec, encode_cat_vars, one_hot = T){
+
+  if(encode_cat_vars == "all"){
+
+    rec <- recipes::step_dummy(rec, recipes::all_factor_predictors(), one_hot = one_hot)
+
+  } else{
+
+    rec <- recipes::step_dummy(rec, all_of(encode_cat_vars), one_hot = one_hot)
+
+  }
+
+  return(rec)
+
+}
+
+encode_target <- function(rec, type_task, dep_var){
+
+  if (type_task == "binary"){
+
+    rec <- recipes::step_dummy(rec, all_of(dep_var))
+
+  } else if (type_task == "multiclass"){
+
+    rec <- recipes::step_dummy(rec, all_of(dep_var), one_hot = T)
+
+  }
+
+  return(rec)
+
+}
 
 preprocessing <- function(df, formula, dep_var, num_vars, cat_vars, norm_num_vars,
                        encode_cat_vars, encode_dep_var){
 
-          rec = recipes::recipe(formula, data = df)
+          rec = create_recipe(formula = formula, data = df)
 
           # Check numerical variables are numeric
 
           if (!is.null(num_var)) {
+
+            ##### TODO: num_vars or norm_num_vars,
+            #### standarize_predictors no usa norm_num_vars
+
               rec <- rec %>% recipes::step_mutate_at(all_of(num_vars),
                                             fn = as.numeric)
           }
@@ -21,53 +72,33 @@ preprocessing <- function(df, formula, dep_var, num_vars, cat_vars, norm_num_var
           # Check categorical variables are factor
 
           if (!is.null(cat_var)) {
+
               rec <- rec %>% recipes::step_mutate_at(all_of(cat_vars),
                                             fn = as.factor)
-
           }
 
           # Normalize selected numerical columns
 
           if (!is.null(norm_num_vars)) {
-            if (norm_num_vars == "all"){
 
-              rec <- rec %>% recipes::step_normalize(all_of(num_vars))
-
-            } else {
-
-              rec <- rec %>% recipes::step_normalize(all_of(norm_num_vars))
-
-            }
+            rec <- standarize(rec = rec, norm_num_vars = norm_num_vars, num_vars = num_vars)
 
           }
 
           # Encode selected categorical columns
 
           if (!is.null(encode_cat_vars)){
-            if(encode_cat_vars == "all"){
 
-              rec <- rec %>% recipes::step_dummy(all_of(cat_vars), one_hot = T)
+              rec <- one_hot_predictors(rec = rec, encode_cat_vars = encode_cat_vars)
 
-            } else{
-
-              rec <- rec %>% recipes::step_dummy(all_of(encode_cat_vars), one_hot = T)
-
-            }
           }
 
           # Encode target variable
 
           if (!is.null(encode_dep_var)){
 
-            if (encode_dep_var == "binary"){
+              rec <- encode_target(rec = rec, type_task = encode_dep_var, dep_var = dep_var)
 
-              rec <- rec %>% recipes::step_dummy(dep_var)
-
-            } else if (encode_dep_var == "multiclass"){
-
-              rec <- rec %>% recipes::step_dummy(dep_var, one_hot = T)
-
-            }
           }
 
           return(rec)
--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -1,18 +1,123 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-
-formula = "y ~ x1 + x2"
+formula = "y ~ x1 + x2 + x3 + x4"
 formula = as.formula(formula)
-y = c(1,0)
-x1 = c(2,3)
-x2 = c(4,5)
-df <- data.frame(y, x1, x2)
+
+formula_multiclass = "y2 ~ x1 + x2 + x3 + x4"
+formula_multiclass = as.formula(formula_multiclass)
+
+df <- data.frame(
+  y = as.factor(c(1,0)),
+  y2 = factor(c(0,2), levels = c(0,1,2)),
+  x1 = c(2,3),
+  x2 = c(4,6),
+  x3 = as.factor(c(0,1)),
+  x4 = factor(c(0,2), levels = c(0,1,2))
+)
 
 test_that("create_recipe produces recipe object", {
 
-
   rec = create_recipe(formula = formula, data = df)
+
   expect_equal(class(rec), 'recipe')
 
+})
+
+test_that("standarize works correctly",{
+
+  rec = create_recipe(formula = formula, data = df)
+
+  # For "all":
+
+  rec_all <- standarize_predictors(rec = rec, norm_num_vars = "all")
+
+  rec_all_prep <- recipes::prep(rec_all, training = df)
+
+  rec_all_bake <- recipes::bake(rec_all_prep, new_data = df)
+
+  expect_equal(rec_all_bake$x1, c(-0.5*sqrt(2), 0.5*sqrt(2)), tolerance=1e-3)
+
+  expect_equal(rec_all_bake$x2, c(-1/sqrt(2), 1/sqrt(2)), tolerance=1e-3)
+
+  # For x1:
+
+  rec_x1 <- standarize_predictors(rec = rec, norm_num_vars = c("x1"))
+
+  rec_x1_prep <- recipes::prep(rec_x1, training = df)
+
+  rec_x1_bake <- recipes::bake(rec_x1_prep, new_data = df)
+
+  expect_equal(rec_x1_bake$x1, c(-0.5*sqrt(2), 0.5*sqrt(2)), tolerance=1e-3)
+
+  expect_equal(rec_x1_bake$x2, df$x2, tolerance=1e-3)
+
+})
+
+test_that("one_hot_predictors works correctly",{
+
+  rec <- create_recipe(formula = formula, data = df)
+
+  # With "all":
+
+  rec_all <- one_hot_predictors(rec = rec, encode_cat_vars = "all")
+
+  rec_all_prep <- recipes::prep(rec_all, training = df)
+
+  rec_all_bake <- recipes::bake(rec_all_prep, new_data = df)
+
+  expect_equal(rec_all_bake$x3_X0, c(1,0))
+
+  expect_equal(rec_all_bake$x3_X1, c(0,1))
+
+  expect_equal(rec_all_bake$x4_X0, c(1,0))
+
+  expect_equal(rec_all_bake$x4_X1, c(0,0))
+
+  expect_equal(rec_all_bake$x4_X2, c(0,1))
+
+  # With only x4:
+
+  rec_all_x4 <- one_hot_predictors(rec = rec, encode_cat_vars = c("x4"))
+
+  rec_all_prep_x4 <- recipes::prep(rec_all_x4, training = df)
+
+  rec_all_bake_x4 <- recipes::bake(rec_all_prep_x4, new_data = df)
+
+  expect_equal(rec_all_bake_x4$x3, df$x3)
+
+  expect_equal(rec_all_bake_x4$x4_X0, c(1,0))
+
+  expect_equal(rec_all_bake_x4$x4_X1, c(0,0))
+
+  expect_equal(rec_all_bake_x4$x4_X2, c(0,1))
+
+})
+
+test_that("encode_target works correctly",{
+
+  # For binary:
+
+  rec_binary <- create_recipe(formula = formula, data = df)
+
+  rec_binary <- encode_target(rec = rec_binary, type_task = "binary", dep_var = "y")
+
+  rec_binary_prep <- recipes::prep(rec_binary, training = df)
+
+  rec_binary_bake <- recipes::bake(rec_binary_prep, new_data = df)
+
+  expect_equal(rec_binary_bake$y_X1, c(1,0))
+
+  # For multiclass:
+
+  rec_multiclass <- create_recipe(formula = formula_multiclass, data = df)
+
+  rec_multiclass <- encode_target(rec = rec_multiclass, type_task = "multiclass", dep_var = "y2")
+
+  rec_multiclass_prep <- recipes::prep(rec_multiclass, training = df)
+
+  rec_multiclass_bake <- recipes::bake(rec_multiclass_prep, new_data = df)
+
+  expect_equal(rec_multiclass_bake$y2_X0, c(1,0))
+
+  expect_equal(rec_multiclass_bake$y2_X1, c(0,0))
+
+  expect_equal(rec_multiclass_bake$y2_X2, c(0,1))
 })
